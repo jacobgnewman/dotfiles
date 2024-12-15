bbe18b439623cd145e002dabcb1a58b78a5953c3
jacobgnewman <57055451+jacobgnewman@users.noreply.github.com>
2024-12-14 18:10:08 -0800

    update flake on mountain

--- nix/machines/mountain/flake.nix
+++ nix/machines/mountain/flake.nix
@@ -5,7 +5,7 @@
       url = "github:NixOS/nixpkgs/nixos-unstable";
     };
     lix-module = {
-      url = "https://git.lix.systems/lix-project/nixos-module/archive/2.91.0.tar.gz";
+      url = "https://git.lix.systems/lix-project/nixos-module/archive/2.91.1-2.tar.gz";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
